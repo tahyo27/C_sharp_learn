c68d3ff02a75b7bebebe403636e838500daf1208
tahyo27 <tahyo27@gmail.com>
2024-10-23 22:59:54 +0900

    c# 섹션 2 완료

---
+++ QuizApp/QuizApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace QuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quiz App!");
+
+            string question1 = "What is the capital of Germany";
+            string answer1 = "Berlin";
+
+            string question2 = "What is 2+2";
+            string answer2 = "4";
+
+            string question3 = "What color do you get by mixing blue and yellow";
+            string answer3 = "Green";
+
+            Console.WriteLine(question1);
+            string userAnswer1 = Console.ReadLine();
+
+            int score = 0;
+
+            if(userAnswer1.Trim().ToLower() == answer1.ToLower())
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine($"Wrong, the correct answer is : {answer1}");
+            }
+            Console.WriteLine(question2);
+            string userAnswer2 = Console.ReadLine();
+
+            if (userAnswer2 == answer2)
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine($"Wrong, the correct answer is : {answer2}");
+            }
+
+            Console.WriteLine(question3);
+            string userAnswer3 = Console.ReadLine();
+
+            if (userAnswer3 == answer3)
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine($"Wrong, the correct answer is : {answer3}");
+            }
+
+            Console.WriteLine($"Quiz completed! Your final score is : {score / 3}");
+
+
+            if(score == 3)
+            {
+                Console.WriteLine("Excellent! You got all the answer right!");
+            }
+            else if(score > 0)
+            {
+                Console.WriteLine("Good Job, but keep learning!");
+            }
+            else
+            {
+                Console.WriteLine("Try again and see if you can get some answers right next time");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ TryParseApp/TryParseApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// creating an instance of the Random Class
+Random random = new Random();
+int randomNumber = random.Next(1, 11);
+
+Console.WriteLine("Give me a number");
+string inputString = Console.ReadLine();
+int num1 = 0;
+bool isNumber = int.TryParse(inputString, out num1);
+if(isNumber)
+{
+    if(num1 == randomNumber)
+    {
+        Console.WriteLine("You guessed right!");
+    }
+    else
+    {
+        Console.WriteLine("You guessed wrong, try again!");
+    }
+    Console.WriteLine("Well done, you entered a number");
+}
+else
+{
+    Console.WriteLine("Haha you troll. You should've enteed a number");
+}
+Console.WriteLine($"User enterd number + 1 {num1}");
+
+Console.ReadKey();

82e4384512965153906b64f3ce3dca5e819830fb
tahyo27 <tahyo27@gmail.com>
2024-10-25 22:00:53 +0900

    섹션 3 완

--- ArrayApp/ArrayApp/Program.cs
+++ ArrayApp/ArrayApp/Program.cs
@@ -1,8 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Array App");
 
+int[,] array2DDeclaration = new int[3, 3]; // [][] 랑 다르게 [,]으로 이차원 배열 선언
+// [0] [0] [0]
+// [0] [0] [0]
+// [0] [0] [0]
+
+int[,,] array3DDeclaration = new int[3, 3, 3];
+
+int[,] array2DInizilized = { {1,2}, { 3, 4 } };
+
+string[,,] simple3DArray =
+{
+    {
+        {"000", "001" },
+        { "010", "011"}
+    },
+    {
+        {"100", "101" },
+        {"110", "111" }
+    }
+};
+
 // 자바랑 똑같이 가능 int[] myIntArray = new int[5];
-int[] myIntArray = [1, 2, 3, 4, 5]; // 자바랑 괄호가 다름
+int[] myIntArray = { 1, 2, 3, 4, 5 }; 
 
 string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
---
+++ MethodApp/MethodApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// Methods are structured like this
+// modifier returnType MoethodName(Parameters) {
+// code block
+// }
+
+//Definition of a void method that returns nothing
+
+void myFirstMethod()
+{
+    Console.WriteLine("MyFistMethod was called");
+}
+
+//Calling a Method
+myFirstMethod();
+
+
+Console.WriteLine("This is outside of the method");
+
+string tmp = "This is temp string";
+void writeSomethingSpecific(string str)
+{
+    Console.WriteLine($"This is writeSomethingSepecific str is {str}");
+}
+writeSomethingSpecific(tmp);
+Console.ReadKey();

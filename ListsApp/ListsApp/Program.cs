4d37ac78a95d298242410a9914b175eeb899ed59
tahyo27 <tahyo27@gmail.com>
2024-10-31 22:14:21 +0900

    섹션 6-1

---
+++ ListsApp/ListsApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lists App");
+
+            //Declaring a list and initializing
+            List<string> colors = new List<string>();
+
+            //Adding item to the list
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("green");
+            Console.WriteLine("Current colors in the colors list!");
+            foreach (string color in colors)
+            {
+
+                Console.WriteLine(color);
+            }
+            bool isDeletingSuccessful = colors.Remove("Red");
+            
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

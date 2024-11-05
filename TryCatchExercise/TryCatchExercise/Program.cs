565afd9177cc49d5ed251f8d0c27da9610b148fe
tahyo27 <tahyo27@gmail.com>
2024-11-05 22:45:30 +0900

    섹션7 예외처리 완

---
+++ TryCatchExercise/TryCatchExercise/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace TryCatchExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try catch exercise");
+
+            Debug.WriteLine("Main method is running");
+        }
+
+        int GetUserAge(string input)
+        {
+            int age;
+            if (!int.TryParse(input, out age))
+            {
+                throw new Exception("You didn't entere a valid age.");
+            }
+            if (age > 0 && age < 120)
+            {
+                throw new Exception("You must between 0 and 120");
+            }
+            return age;
+        }
+
+        void PrintTryCatch()
+        {
+            try
+            {
+                Console.WriteLine("Trying...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally executed.");
+            }
+        }
+
+        public void HandleMultipleExceptions(string numberString, int index)
+        {
+            // TODO
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int number = int.Parse(numberString);
+                Console.WriteLine(numbers[index]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range.");
+            }
+        }
+    }
+}

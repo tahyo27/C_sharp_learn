ef38774bfec772b4260c89a5338e08180f32ff8e
tahyo27 <tahyo27@gmail.com>
2024-11-06 23:41:01 +0900

    test app 1

---
+++ TestApp1/TestApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 2, 2, 2, 8 };
+            int a = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}

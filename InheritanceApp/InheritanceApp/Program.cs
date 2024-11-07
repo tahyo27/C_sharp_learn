9a02759a6d60868869cc14cf1436e1a1e0e94058
tahyo27 <tahyo27@gmail.com>
2024-11-07 22:27:20 +0900

    섹션 7 상속 1

---
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace InheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Bark();
+            myDog.Eat();
+            Console.WriteLine("Inheritance App!");
+            
+        }
+    }
+
+    // Base Class ( Parent Class or Superclass)
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking.....");
+        }
+    }
+
+    // A breed of dog
+    class Cat : Animal
+    {
+        public void Miau()
+        {
+            Console.WriteLine("Cat is meowing");
+        }
+    }
+
+}

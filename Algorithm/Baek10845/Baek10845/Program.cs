eb42036690284d44fdce18cf43ddd194b44c3b02
tahyo27 <tahyo27@gmail.com>
2024-11-10 20:38:12 +0900

    baek10845 큐 구현

---
+++ Algorithm/Baek10845/Baek10845/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Baek10845
+{
+    internal class Program
+    {
+        private static LinkedList<int> list = new LinkedList<int>();
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string str = Console.ReadLine();
+                if(str.Contains("push"))
+                {
+                    int num = int.Parse(str.Substring(5));
+                    push(num);
+                }
+                else if(str.Contains("front"))
+                {
+                    front();
+                }
+                else if (str.Contains("pop"))
+                {
+                    pop();
+                }
+                else if (str.Contains("back"))
+                {
+                    back();
+                }
+                else if (str.Contains("size"))
+                {
+                    size();
+                }
+                else if (str.Contains("empty"))
+                {
+                    empty();
+                }
+            }
+
+        }
+
+        public static void push(int x)
+        {
+            list.AddLast(x);
+        }
+
+        public static void pop()
+        {
+            if(list.First != null)
+            {
+                Console.WriteLine(list.First.Value);
+                list.RemoveFirst();
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+        public static void size()
+        {
+            Console.WriteLine(list.Count);
+        }
+        public static void empty()
+        {
+            if (list.Count == 0)
+            { 
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+        public static void front()
+        {
+            if (list.First != null)
+            {
+                Console.WriteLine(list.First.Value);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+        public static void back()
+        {
+            if(list.Last != null)
+            {
+                Console.WriteLine(list.Last.Value);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+
+
+
+    }
+}
---
+++ Algorithm/Baek10845/Baek10845/Program2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baek10845
+{
+    internal class Program2
+    {
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            Queue<int> queue = new Queue<int>();
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                switch (command[0])
+                {
+                    case "push":
+                        int x = int.Parse(command[1]);
+                        queue.Enqueue(x);
+                        break;
+
+                    case "pop":
+                        if (queue.Count > 0)
+                            results.Add(queue.Dequeue().ToString());
+                        else
+                            results.Add("-1");
+                        break;
+
+                    case "size":
+                        results.Add(queue.Count.ToString());
+                        break;
+
+                    case "empty":
+                        results.Add(queue.Count == 0 ? "1" : "0");
+                        break;
+
+                    case "front":
+                        if (queue.Count > 0)
+                            results.Add(queue.Peek().ToString());
+                        else
+                            results.Add("-1");
+                        break;
+
+                    case "back":
+                        if (queue.Count > 0)
+                            results.Add(queue.ToArray()[queue.Count - 1].ToString());
+                        else
+                            results.Add("-1");
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", results));
+        }
+
+    }
+}

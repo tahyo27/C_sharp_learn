f3abebc5e24bf010340fe0fe1272fabe0a72e280
tahyo27 <tahyo27@gmail.com>
2024-11-14 22:29:53 +0900

    섹션 11-1

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegatesAndEvents
+{
+    internal class Program
+    {
+
+        public delegate void Notify(string message);
+        static void Main(string[] args)
+        {
+            Notify notifyDelgate = ShowMessage;
+
+            notifyDelgate("Hello Delegates!");
+
+            Console.ReadKey();
+        }
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    
+}

006d67c6b20a13102425f9cdaaf511d7a0733efa
tahyo27 <tahyo27@gmail.com>
2024-11-15 22:21:38 +0900

    섹션 11-2

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,7 +1,23 @@
 ﻿namespace DelegatesAndEvents
 {
+
+    public delegate void LogHandler(string message);
     internal class Program
     {
+        
+
+        public class Logger
+        {
+            public void LogToConsoe(string message)
+            {
+                Console.WriteLine($"Console Log : {message}");
+            }
+
+            public void LogToFile(string message)
+            {
+                Console.WriteLine("File Log: " + message);
+            }
+        }
 
         public delegate void Notify(string message);
         static void Main(string[] args)
@@ -9,6 +25,16 @@
             Notify notifyDelgate = ShowMessage;
 
             notifyDelgate("Hello Delegates!");
+
+            Logger logger = new Logger();
+            LogHandler logHandler = logger.LogToConsoe;
+            logHandler("Logging to console");
+
+            logHandler = logger.LogToFile;
+            logHandler("Logging to File");
+
+
+
 
             Console.ReadKey();
         }

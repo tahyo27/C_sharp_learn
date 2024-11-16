f22e8b0f067c14e8e739bb2860e3b39ff06875c7
tahyo27 <tahyo27@gmail.com>
2024-11-16 21:34:44 +0900

    11 - 3

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace DelegatesAndEvents
 {
+    public delegate int Comparison<T>(T x, T y);
 
     public delegate void LogHandler(string message);
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonSorter
+    {
+        public void Sort(Person[] people, Comparison<Person> comparison)
+        {
+            for(int i = 0; i < people.Length -1; i++)
+            {
+                for(int j = i + 1; j < people.Length; j++)
+                {
+                    if (comparison(people[i], people[j]) > 0)
+                    {
+                        Person temp = people[i];
+                        people[i] = people[j];
+                        people[j] = temp;
+                    }
+                }
+            }
+
+        }
+    }
     internal class Program
     {
-        
+
 
         public class Logger
         {
@@ -30,18 +57,50 @@
             LogHandler logHandler = logger.LogToConsoe;
             logHandler("Logging to console");
 
-            logHandler = logger.LogToFile;
+            logHandler += logger.LogToFile;
             logHandler("Logging to File");
 
+            Person[] people =
+            {
+                new Person {Name = "Alice", Age = 30 },
+                new Person {Name = "John", Age = 25 },
+                new Person {Name = "Charlie", Age = 35}
+            };
 
+            PersonSorter sorter = new PersonSorter();
+            sorter.Sort(people, CompareByAge);
 
+            foreach (Person person in people)
+            {
+                Console.WriteLine($">>>>>>>>> Name : {person.Name}, Age : {person.Age}");
+            }
+
+            InvokeSafely(logHandler, "After removing LogToFile");
+           // logHandler("After removing LogToFile");
 
             Console.ReadKey();
+        }
+
+        static int CompareByAge(Person x, Person y)
+        {
+            return x.Age.CompareTo(y.Age);
         }
         static void ShowMessage(string message)
         {
             Console.WriteLine(message);
         }
+
+
+        static void InvokeSafely(LogHandler logHandler, string message)
+        {
+            LogHandler tempLogHandler = logHandler;
+            if(tempLogHandler != null)
+            {
+                tempLogHandler(message);
+            }
+        }
+
+
     }
     
 }

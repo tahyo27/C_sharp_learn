244d70f2417daecf82365887860d048ab135744a
tahyo27 <tahyo27@gmail.com>
2024-11-18 21:54:55 +0900

    섹션 12 정규식

---
+++ RegexApp/RegexApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regex");
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, my number is 12314";
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found:\n {1}", matches.Count, text);
+
+            foreach(Match hit in matches)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+}

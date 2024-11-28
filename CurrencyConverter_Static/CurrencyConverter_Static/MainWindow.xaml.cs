b44f1e7e386ef414c38ed925eb68d6cfb8a8de2e
tahyo27 <tahyo27@gmail.com>
2024-11-28 22:03:50 +0900

    wpf 컨버터 -2

--- CurrencyConverter_Static/CurrencyConverter_Static/MainWindow.xaml.cs
+++ CurrencyConverter_Static/CurrencyConverter_Static/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace CurrencyConverter_Static
         public MainWindow()
         {
             InitializeComponent();
+            lblCurrency.Content = "Hello world";
         }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+
+            //Add display column in DataTable
+            dtCurrency.Columns.Add("Text");
+
+            //Add value column in DataTable
+            dtCurrency.Columns.Add("Value");
+
+            //Add rows in Datatable with text and value
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("INR", 1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR", 20);
+            dtCurrency.Rows.Add("POUND", 5);
+            dtCurrency.Rows.Add("DEM", 43);
+
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            lblCurrency.Content = "Hello world";
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            lblCurrency.Content = "";
+        }
+
+        private void NumberValidationTextBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
     }
 }

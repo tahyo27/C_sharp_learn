0c458e53f3fdc2135e4dd4d0f14ff8f26911c01e
tahyo27 <tahyo27@gmail.com>
2024-12-08 18:23:52 +0900

    Linq - 1

---
+++ Linq1/Linq1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            OddNumbers(numbers);
+
+            Console.ReadKey();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers:");
+
+            IEnumerable<int> oddNumbers = from number in numbers
+                                          where number % 2 != 0
+                                          select number;
+            Console.WriteLine(oddNumbers);
+
+            foreach (int number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

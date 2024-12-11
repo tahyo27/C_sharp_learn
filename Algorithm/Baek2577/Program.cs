a6ce5ea5d1f97f36f8f95aa31deea49a709f4992
tahyo27 <tahyo27@gmail.com>
2024-12-11 21:23:55 +0900

    Baek 2577

---
+++ Algorithm/Baek2577/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Baek2577
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nA = int.Parse(Console.ReadLine());
+            int nB = int.Parse(Console.ReadLine());
+            int nC = int.Parse(Console.ReadLine());
+            int[] numCount = new int[10];
+
+            int multiResult = nA * nB * nC;
+
+            while (multiResult > 0)
+            {
+                int idx = multiResult % 10;
+                numCount[idx]++;
+                multiResult /= 10;
+            }
+
+            foreach (int i in numCount)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

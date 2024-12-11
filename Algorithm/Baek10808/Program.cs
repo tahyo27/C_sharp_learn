01a8d51af17a0abc7675c5e10b2c2910baf4e3f0
tahyo27 <tahyo27@gmail.com>
2024-12-11 21:21:12 +0900

    Baek 10808

---
+++ Algorithm/Baek10808/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Baek10808
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] alpha = new int[26];
+            string input = Console.ReadLine();
+
+            char[] cInput = input.ToCharArray();
+
+            for (int i = 0; i < cInput.Length; i++)
+            {
+                int idx = cInput[i] - 97;
+                alpha[idx] += 1;
+            }
+            foreach (int i in alpha)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}

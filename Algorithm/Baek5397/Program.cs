71a02677bd943bbd8de51e2f434df9d8a8b13554
tahyo27 <tahyo27@gmail.com>
2024-12-18 21:01:30 +0900

    Baek 5397

---
+++ Algorithm/Baek5397/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Baek5397
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nN = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nN; i++)
+            {
+                string sInput = Console.ReadLine();
+                char[] cInput = sInput.ToCharArray();
+                LinkedList<char> cList = new LinkedList<char>();
+                LinkedListNode<char> cursor = cList.AddLast(' ');
+                foreach (char c in cInput)
+                {
+                    switch (c)
+                    {
+                        case '<':
+                            if (cursor.Previous != null)
+                            {
+                                cursor = cursor.Previous;
+                            }
+                            break;
+
+                        case '>':
+                            if (cursor.Next != null)
+                            {
+                                cursor = cursor.Next;
+                            }
+                            break;
+                        case '-':
+                            if (cursor.Previous != null)
+                            {
+                                cursor = cursor.Previous;
+                                cList.Remove(cursor.Next);
+                            }
+                            break;
+                        default:
+                            cList.AddAfter(cursor, c);
+                            cursor = cursor.Next;
+                            break;
+                    }
+                }
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in cList)
+                {
+                    sb.Append(c);
+                }
+
+                Console.WriteLine(sb.ToString().Trim());
+            }
+        }
+    }
+}

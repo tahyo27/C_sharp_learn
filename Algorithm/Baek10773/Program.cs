420fa98742b5a09485bdeaf93f0e2f66bbafdf0b
tahyo27 <tahyo27@gmail.com>
2024-12-22 22:21:14 +0900

    Baek 10773 스택

---
+++ Algorithm/Baek10773/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Baek10773
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nK = int.Parse(Console.ReadLine());
+            Stack<int> st = new Stack<int>();
+            int sum = 0;
+
+            for (int i = 0; i < nK; i++)
+            {
+                int nInput = int.Parse(Console.ReadLine());
+                if (nInput == 0)
+                {
+                    st.TryPop(out int result);
+                }
+                else
+                {
+                    st.Push(nInput);
+                }
+            }
+
+            while (st.Count > 0)
+            {
+                sum += st.Pop();
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

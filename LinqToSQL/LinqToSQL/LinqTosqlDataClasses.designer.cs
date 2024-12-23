016192ccc5634ee709592965fdcb4dda458e0cbe
tahyo27 <tahyo27@gmail.com>
2024-12-23 22:11:29 +0900

    LinqToSql 인서트부분

--- LinqToSQL/LinqToSQL/LinqTosqlDataClasses.designer.cs
+++ LinqToSQL/LinqToSQL/LinqTosqlDataClasses.designer.cs
@@ -22,6 +22,7 @@ namespace LinqToSQL
 	using System;
 	
 	
+	[global::System.Data.Linq.Mapping.DatabaseAttribute(Name="TestDB")]
 	public partial class LinqTosqlDataClassesDataContext : System.Data.Linq.DataContext
 	{
 		
@@ -29,7 +30,19 @@ namespace LinqToSQL
 		
     #region 확장성 메서드 정의
     partial void OnCreated();
+    partial void InsertUniversity(University instance);
+    partial void UpdateUniversity(University instance);
+    partial void DeleteUniversity(University instance);
+    partial void InsertStudent(Student instance);
+    partial void UpdateStudent(Student instance);
+    partial void DeleteStudent(Student instance);
     #endregion
+		
+		public LinqTosqlDataClassesDataContext() : 
+				base(global::LinqToSQL.Properties.Settings.Default.TestDBConnectionString, mappingSource)
+		{
+			OnCreated();
+		}
 		
 		public LinqTosqlDataClassesDataContext(string connection) : 
 				base(connection, mappingSource)
@@ -53,6 +66,311 @@ namespace LinqToSQL
 				base(connection, mappingSource)
 		{
 			OnCreated();
+		}
+		
+		public System.Data.Linq.Table<University> University
+		{
+			get
+			{
+				return this.GetTable<University>();
+			}
+		}
+		
+		public System.Data.Linq.Table<Student> Student
+		{
+			get
+			{
+				return this.GetTable<Student>();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.University")]
+	public partial class University : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private string _Name;
+		
+		private EntitySet<Student> _Student;
+		
+    #region 확장성 메서드 정의
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    #endregion
+		
+		public University()
+		{
+			this._Student = new EntitySet<Student>(new Action<Student>(this.attach_Student), new Action<Student>(this.detach_Student));
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="University_Student", Storage="_Student", ThisKey="Id", OtherKey="UniversityId")]
+		public EntitySet<Student> Student
+		{
+			get
+			{
+				return this._Student;
+			}
+			set
+			{
+				this._Student.Assign(value);
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		
+		private void attach_Student(Student entity)
+		{
+			this.SendPropertyChanging();
+			entity.University = this;
+		}
+		
+		private void detach_Student(Student entity)
+		{
+			this.SendPropertyChanging();
+			entity.University = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Student")]
+	public partial class Student : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private string _Name;
+		
+		private string _Gender;
+		
+		private int _UniversityId;
+		
+		private EntityRef<University> _University;
+		
+    #region 확장성 메서드 정의
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    partial void OnGenderChanging(string value);
+    partial void OnGenderChanged();
+    partial void OnUniversityIdChanging(int value);
+    partial void OnUniversityIdChanged();
+    #endregion
+		
+		public Student()
+		{
+			this._University = default(EntityRef<University>);
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Gender", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Gender
+		{
+			get
+			{
+				return this._Gender;
+			}
+			set
+			{
+				if ((this._Gender != value))
+				{
+					this.OnGenderChanging(value);
+					this.SendPropertyChanging();
+					this._Gender = value;
+					this.SendPropertyChanged("Gender");
+					this.OnGenderChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_UniversityId", DbType="Int NOT NULL")]
+		public int UniversityId
+		{
+			get
+			{
+				return this._UniversityId;
+			}
+			set
+			{
+				if ((this._UniversityId != value))
+				{
+					if (this._University.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnUniversityIdChanging(value);
+					this.SendPropertyChanging();
+					this._UniversityId = value;
+					this.SendPropertyChanged("UniversityId");
+					this.OnUniversityIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="University_Student", Storage="_University", ThisKey="UniversityId", OtherKey="Id", IsForeignKey=true, DeleteOnNull=true, DeleteRule="CASCADE")]
+		public University University
+		{
+			get
+			{
+				return this._University.Entity;
+			}
+			set
+			{
+				University previousValue = this._University.Entity;
+				if (((previousValue != value) 
+							|| (this._University.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._University.Entity = null;
+						previousValue.Student.Remove(this);
+					}
+					this._University.Entity = value;
+					if ((value != null))
+					{
+						value.Student.Add(this);
+						this._UniversityId = value.Id;
+					}
+					else
+					{
+						this._UniversityId = default(int);
+					}
+					this.SendPropertyChanged("University");
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
 		}
 	}
 }
--- LinqToSQL/LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using System.Configuration;
+using LinqToSQL.TestDBDataSetTableAdapters;
 
 namespace LinqToSQL
 {
@@ -22,13 +23,51 @@ namespace LinqToSQL
     /// </summary>
     public partial class MainWindow : Window
     {
-        LinqTosqlDataClassesDataContext dataContext
+        LinqTosqlDataClassesDataContext dataContext;
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.TestDBConnectionString"].ConnectionString;
             dataContext = new LinqTosqlDataClassesDataContext(connectionString);
+
+            //InsertUniversities();
+            InsertStudent();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("DELETE FROM University");
+
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.University.InsertOnSubmit(yale);
+
+            University beijingTech = new University();
+            beijingTech.Name = "Beijing Tech";
+            dataContext.University.InsertOnSubmit(beijingTech);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.University;
+        }
+
+        public void InsertStudent()
+        {
+            University yale = dataContext.University.First(un => un.Name.Equals("Yale"));
+            University beijingTech = dataContext.University.First(un => un.Name.Equals("Beijing Tech"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Tonie", Gender = "male", University = yale});
+            students.Add(new Student { Name = "Leyle", Gender = "female", UniversityId = beijingTech.Id });
+            students.Add(new Student { Name = "James", Gender = "trans-gender", University = beijingTech });
+
+            dataContext.Student.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Student;
         }
     }
 }

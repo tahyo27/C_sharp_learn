1420587ac9c511b452f96eab17306b81303bf179
tahyo27 <tahyo27@gmail.com>
2025-01-02 21:02:27 +0900

    Baek 3986 스택

---
+++ Algorithm/Baek 3986/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Baek_3986
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                Stack<char> st = new Stack<char>();
+
+                foreach (char c in input)
+                {
+                    if (st.Count != 0 && st.Peek().Equals(c))
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        st.Push(c);
+                    }
+                }
+                if (st.Count == 0)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

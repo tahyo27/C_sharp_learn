265c428e0203b4ec8099e652e380c0442f53d9e2
tahyo27 <tahyo27@gmail.com>
2025-01-03 21:00:29 +0900

    Baek 10799 스택풀이

---
+++ Algorithm/Baek 10799/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Baek_10799
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> st = new Stack<char>();
+            int count = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals('('))
+                {
+                    st.Push(input[i]);
+                }
+                else
+                {
+                    if (input[i - 1] == '(')
+                    {
+                        st.Pop();
+                        count += st.Count;
+                    }
+                    else
+                    {
+                        st.Pop();
+                        count++; // 앞에 레이저 있어서 잘린부분 추가
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}

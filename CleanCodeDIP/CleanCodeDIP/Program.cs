7b3f6d2f86bf0ecca2ede4a45db7fd67a4c9d6c2
tahyo27 <tahyo27@gmail.com>
2025-01-06 21:52:06 +0900

    C# Clean code DIP

---
+++ CleanCodeDIP/CleanCodeDIP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CleanCodeDIP
+{
+    internal class Program
+    {
+        //DIP 
+        // 추상화에 의존해야지 구체화에 의존하면 안된다
+        //자신보다 변화하기 쉬운 것을 의존해서는 안 되고, 거의 변화가 없는 개념에 의존해야 한다
+        static void Main(string[] args)
+        {
+            IEmailService emailService = new EmailService();
+            Notification notification = new Notification(emailService);
+            notification.Send("Hello, this is a test notification");
+        }
+    }
+
+    public interface IEmailService
+    {
+        public void SendEmail(string to, string subject, string body);
+    }
+
+   public class EmailService : IEmailService
+   {
+        public void SendEmail(string to, string subject, string body)
+        {
+            Console.WriteLine($"Sending email to {to} with subject {subject}");
+        }
+   }
+
+    public class Notification
+    {
+        private readonly IEmailService _emailService;
+        //private readonly EmailService _emailService;
+
+        //public Notification() 이렇게 상위가 구체적인것에 의존하면 안됨
+        //{
+        //    _emailService = new EmailService();
+        //}
+        public Notification(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+        public void Send(string message)
+        {
+            _emailService.SendEmail("user@example.com", "Notification", message);
+        }
+    }
+
+}

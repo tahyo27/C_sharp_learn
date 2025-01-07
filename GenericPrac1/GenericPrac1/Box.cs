28d54a0a6d1eb328971f6a925ffda51546038a4d
tahyo27 <tahyo27@gmail.com>
2025-01-07 21:55:15 +0900

    C# generic - 1

---
+++ GenericPrac1/GenericPrac1/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrac1
+{
+    internal class Box<T>
+    {
+        //public T Content { get; set; }
+
+        //public string Log()
+        //{
+        //    return $"Box contains {Content}";
+        //}
+
+        private T content;
+        
+        public Box(T initialValue)
+        {
+            content = initialValue;
+        }
+
+        public void UpdateContent(T newContent)
+        {
+            content = newContent;
+            Console.WriteLine($"Update content to {content}");
+        }
+
+        public T GetContent()
+        {
+            return content;
+        }
+    }
+}
---
+++ GenericPrac1/GenericPrac1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericPrac1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Box<int> box = new Box<int>();
+            //box.Content = 1;
+            //Console.WriteLine(box.Log());
+
+            //Box<string> boxStr = new Box<string>();
+            //boxStr.Content = "Hello World";
+            //Console.WriteLine(boxStr.Log());
+
+            Box<string> boxStr = new Box<string>("Hello World");
+            boxStr.UpdateContent("Teaching C# is fun");
+            Console.WriteLine(boxStr.GetContent());
+        }
+    }
+}

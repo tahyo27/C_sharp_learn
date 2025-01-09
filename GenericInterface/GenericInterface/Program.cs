2d4e5c7746e88832fc82b089ac2f3acb53754af1
tahyo27 <tahyo27@gmail.com>
2025-01-09 21:55:08 +0900

    C# interface generic

---
+++ GenericInterface/GenericInterface/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace GenericInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    internal interface IEntity
+    {
+        int Id { get; }
+    }
+
+    internal interface IRepository<T> where T : IEntity
+    {
+        void Add(T entity);
+        void Remove(T entity);
+    }
+    internal class User : IEntity
+    {
+        public string Name { get; set; }
+
+        public int Id => throw new NotImplementedException();
+    }
+
+    internal class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class ProductRepository : IRepository<Product>
+    {
+        public void Add(Product entity)
+        {
+
+        }
+        public void Remove(Product entity)
+        {
+
+        }
+    }
+
+    internal class UserRepository : IRepository<User>
+    {
+        public void Add(User entity)
+        {
+
+        }
+        public void Remove(User entity)
+        {
+
+        }
+    }
+
+
+}

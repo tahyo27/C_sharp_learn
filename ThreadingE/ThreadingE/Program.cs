685994ed2fe9dc69167449b0d592d4aed117933c
tahyo27 <tahyo27@gmail.com>
2025-01-12 21:20:09 +0900

    C# Thread - 1

---
+++ ThreadingE/ThreadingE/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ThreadingE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World! 1");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello, World! 2");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello, World! 3");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello, World! 4");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello, World! 5");
+
+            new Thread(() => 
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+            var test = taskCompletionSource.Task.Result;
+
+            Console.ReadKey();
+        }
+    }
+}

753484d62197ff6a6f51323113c7ec9d7b6d1e40
tahyo27 <tahyo27@gmail.com>
2025-01-13 21:35:46 +0900

    C# Thread - 2

--- ThreadingE/ThreadingE/Program.cs
+++ ThreadingE/ThreadingE/Program.cs
@@ -14,38 +14,62 @@
             //Thread.Sleep(1000);
             //Console.WriteLine("Hello, World! 5");
 
-            new Thread(() => 
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
+            //new Thread(() => 
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 1");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 1");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 2");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 3");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 4");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 4");
+            //}).Start();
 
-            var taskCompletionSource = new TaskCompletionSource<bool>();
-            var test = taskCompletionSource.Task.Result;
+            //var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            //var thread = new Thread(() =>
+            //{
+            //    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+            //    Thread.Sleep(5000);
+            //    taskCompletionSource.TrySetResult(true);
+            //    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+            //});
+            //thread.Start();
+            //var test = taskCompletionSource.Task.Result;
+            //Console.WriteLine($"task was done: {test}");
+
+            Enumerable.Range(0, 100).ToList().ForEach(f =>
+            {
+                ThreadPool.QueueUserWorkItem((o) =>
+                {
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+
+                });
+            });
+
+            
+
 
             Console.ReadKey();
         }

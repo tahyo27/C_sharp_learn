bb300961c5df9c07d135a364c630106ba0e460ee
tahyo27 <tahyo27@gmail.com>
2025-01-14 22:52:07 +0900

    C# Thread 강의완

---
+++ ThreadJoiningE/ThreadJoiningE/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ThreadJoiningE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main Thread started");
+
+            Thread thread1 = new Thread(Thread1Function);
+            Thread thread2 = new Thread(Thread2Function);
+            thread1.Start();
+            thread2.Start();
+
+            //thread1.Join(); // thread가 완료될때까지 기다리게함 비동기일때 순서 제어 용도로 쓰는듯
+            //Console.WriteLine("Thread1Function done");
+           
+
+            if (thread1.Join(1000))
+            {
+                Console.WriteLine("Thread1Function done");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Functino wasn't done within 1 sec");
+            }
+
+            thread2.Join();
+            Console.WriteLine("Thread2Function done");
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (thread1.IsAlive)
+                {
+                    Console.WriteLine("Thread1 is still doing stuff");
+                    Thread.Sleep(300);
+                }
+                else
+                {
+                    Console.WriteLine("Thread1 completed");
+                }
+            }
+            
+
+            Console.WriteLine("Main Thread ended");
+        }
+
+        public static void Thread1Function()
+        {
+            Console.WriteLine("Thread1Function started");
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread1Function coming back to caller");
+        }
+
+        public static void Thread2Function()
+        {
+            Console.WriteLine("Thread2Function started");
+        }
+    }
+}
---
+++ WPFTasksE/WPFTasksE/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTasksE
+{
+    
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty HtmlPropery = DependencyProperty.RegisterAttached(
+        "Html",
+        typeof(string),
+        typeof(MainWindow),
+        new FrameworkPropertyMetadata(OnHtmlChanged)
+        );
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Hi");
+            Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId}");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+
+                MyButton.Dispatcher.Invoke(() =>
+                {
+                    Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} own myButton");
+                    MyButton.Content = "Done";
+                });
+               
+            });
+            
+        }
+
+        private async void MyButton_Click2(object sender, RoutedEventArgs e)
+        {
+            string myHtml = "Bla";
+            // MessageBox.Show("Hi");
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} before await task");
+
+            await Task.Run(async() =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} during await task");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+                myHtml = html;
+            });
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} after await task");
+            MyButton.Content = "Done Downloading";
+            MyWebBrowser.SetValue(HtmlPropery, myHtml);
+
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            if(webBrowser != null)
+            {
+                webBrowser.NavigateToString(e.NewValue as string);
+            }
+        }
+    }
+}

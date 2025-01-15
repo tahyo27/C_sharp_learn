6e8500f1d2c362312bd8f82f2ab85a8ab995aa52
tahyo27 <tahyo27@gmail.com>
2025-01-15 21:50:25 +0900

    C# TDD 기초 세팅

---
+++ Calculator/Calculator/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Domain;
+
+namespace CalculatorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var calculator = new Calculator();
+
+            if(calculator.Sum(2, 2) != 4)
+            {
+                throw new Exception();
+            }    
+        }
+
+       
+    }
+}
---
+++ Calculator/Domain/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Calculator
+    {
+        public int Sum(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}

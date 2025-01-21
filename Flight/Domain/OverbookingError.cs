6c39305a0f8cccfa9a593b5fed8ebf02480babba
tahyo27 <tahyo27@gmail.com>
2025-01-21 21:21:01 +0900

    C# TDD -4

--- Flight/Domain/OverbookingError.cs
+++ Flight/Domain/OverbookingError.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain
 {
-    public class Class1
+    public class OverbookingError
     {
 
     }
--- Flight/Flight/FlightSpecifications.cs
+++ Flight/Flight/FlightSpecifications.cs
@@ -12,5 +12,18 @@ namespace FlightTest
             flight.Book("jannick@tutorialeu.com", 1); // 2.
             flight.RemainingNumberOfSeats.Should().Be(2); // 3.
         }
+
+        [Fact]
+        public void Avoids_overbooking()
+        {
+            // Given
+            var flight = new Flight(seatCapacity: 3);
+
+            // When
+            var error = flight.Book("jannick@tutorialseu.com", 4);
+
+            // Then
+            error.Should().BeOfType<OverbookingError>();
+        }
     }
 }

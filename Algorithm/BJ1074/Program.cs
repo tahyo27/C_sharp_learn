e1f4e89d78c692b774458870c524d8aa66506f6d
tahyo27 <tahyo27@gmail.com>
2025-01-22 17:07:52 +0900

    Baek 1074

---
+++ Algorithm/BJ1074/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BJ1074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int N = input[0];
+            int r = input[1];
+            int c = input[2];
+
+            // int result = Search(N, r, c);
+            Console.WriteLine(Search(N, r, c));
+        }
+
+        static int Search(int n, int r, int c)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            int half = (int)Math.Pow(2, n - 1);
+            if (r < half && c < half)
+            {
+                return Search(n - 1, r, c);
+            }
+            if (r < half && c >= half)
+            {
+                return half * half + Search(n - 1, r, c - half);
+            }
+            if (r >= half && c < half)
+            {
+                return 2 * half * half + Search(n - 1, r - half, c);
+            }
+            return 3 * half * half + Search(n - 1, r - half, c - half);
+        }
+    }
+    
+}

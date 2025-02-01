7a543629f1d3a35da007162321d80dabee4f1a7c
tahyo27 <tahyo27@gmail.com>
2025-02-01 22:30:43 +0900

    C# TDD Flight application layer -1

--- Flight/Application.tests/FlightApplicationSpecification.cs
+++ Flight/Application.tests/FlightApplicationSpecification.cs
@@ -1,11 +1,41 @@
+using FluentAssertions;
+
 namespace Application.tests
+
 {
     public class FlightApplicationSpecification
     {
         [Fact]
         public void Books_flight()
         {
+            var bookingService = new BookingService();
+            bookingService.Book(new BookDto());
+            bookingService.FindBookings().Should().ContainEquivalentOf(
+                new BookingRm()
+                );
+        }
+    }
+
+    public class BookingService
+    {
+        public void Book(BookDto bookDto)
+        {
 
         }
+
+        public IEnumerable<BookingRm> FindBookings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BookDto
+    {
+
+    }
+    
+    public class BookingRm
+    {
+
     }
 }

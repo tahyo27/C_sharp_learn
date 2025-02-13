993de3994eb388fe1122e5232f4081f72499c84e
tahyo27 <tahyo27@gmail.com>
2025-02-13 21:53:11 +0900

    C# Diary Model write

---
+++ Diary/DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

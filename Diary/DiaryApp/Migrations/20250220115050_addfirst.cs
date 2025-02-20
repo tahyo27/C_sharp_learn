f07ef7bb49f394cfb86f986a89e8ee0f509ffcde
tahyo27 <tahyo27@gmail.com>
2025-02-20 22:10:52 +0900

    C# ASP.NET Diary DB 연결 오류 수정

---
+++ Diary/DiaryApp/Migrations/20250220115050_addfirst.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfirst : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaryEntries", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "CreatedDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2025, 2, 19, 13, 10, 30, 0, DateTimeKind.Unspecified), "Went Hiking" },
+                    { 2, "Went shopping with Joe!", new DateTime(2025, 2, 19, 14, 10, 30, 0, DateTimeKind.Unspecified), "Went Shopping" },
+                    { 3, "Went diving with Joe!", new DateTime(2025, 2, 19, 15, 10, 30, 0, DateTimeKind.Unspecified), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaryEntries");
+        }
+    }
+}

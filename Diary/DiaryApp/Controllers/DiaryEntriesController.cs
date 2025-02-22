af8eaa2718229d488188283a230808dcbdcccfa8
tahyo27 <tahyo27@gmail.com>
2025-02-22 20:54:38 +0900

    C# Diary add controller

--- Diary/DiaryApp/Controllers/DiaryEntriesController.cs
+++ Diary/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
+            return View(objDiaryEntryList);
         }
     }
 }

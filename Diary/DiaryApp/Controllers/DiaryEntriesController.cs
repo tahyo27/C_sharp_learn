2c3adbac5e0497d8c6a852c5e1c67782210a8561
tahyo27 <tahyo27@gmail.com>
2025-02-28 22:36:15 +0900

    C# ASP.NET Diary create and storing in the DB

--- Diary/DiaryApp/Controllers/DiaryEntriesController.cs
+++ Diary/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -22,5 +22,13 @@ namespace DiaryApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(DiaryEntry obj)
+        {
+            _db.DiaryEntries.Add(obj);  //새로운 Diary Entriy를 데이터베이스에 추가
+            _db.SaveChanges();          // 변경점을 데이터베이스에 저장
+            return RedirectToAction("Index", "Home");
+        }
     }
 }

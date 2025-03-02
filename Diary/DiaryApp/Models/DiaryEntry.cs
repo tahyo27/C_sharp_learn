08fe94ca52f33ce81142406902513c441147a1c4
tahyo27 <tahyo27@gmail.com>
2025-03-02 21:46:18 +0900

    C# ASP.NET validation and Error message

--- Diary/DiaryApp/Models/DiaryEntry.cs
+++ Diary/DiaryApp/Models/DiaryEntry.cs
@@ -6,7 +6,8 @@ namespace DiaryApp.Models
     {
         //[Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a title!")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 Characters!")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;

02dc4595ba1779f9d816859e437e4046c30f9ca1
tahyo27 <tahyo27@gmail.com>
2025-03-11 21:04:21 +0900

    C# DiaryAPI 프로젝트 swagger 세팅

--- WebDiaryAPI/WebDiaryAPI/Controllers/WeatherForecastController.cs
+++ WebDiaryAPI/WebDiaryAPI/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace WebDiaryAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- WebDiaryAPI/WebDiaryAPI/Program.cs
+++ WebDiaryAPI/WebDiaryAPI/Program.cs
@@ -1,17 +1,29 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+if(app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPI V1");
+    });
 }
 
 app.UseHttpsRedirection();

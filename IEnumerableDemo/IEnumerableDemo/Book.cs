8cfb02d2132d24055a14706c8033df3d4eeada40
tahyo27 <tahyo27@gmail.com>
2025-03-15 15:02:18 +0900

    C# IEnumerable

---
+++ IEnumerableDemo/IEnumerableDemo/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDemo
+{
+    public class Book
+    {
+        public string Title { get; set; }
+    }
+    
+    public class Library
+    {
+        // Method that accepts any IEnumerable<string> and prints each title
+
+        public void PrintBookTitles(IEnumerable<string> bookTitles)
+        {
+            foreach (string title in bookTitles)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+
+}

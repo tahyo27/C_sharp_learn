378161d5142a49f5ed5857b7c51dd7e2813ea5a2
tahyo27 <tahyo27@gmail.com>
2025-04-10 21:52:25 +0900

    C# WPF Contact refactoring

--- DesktopContactsApp/DesktopContactsApp/NewContactsWindow.xaml.cs
+++ DesktopContactsApp/DesktopContactsApp/NewContactsWindow.xaml.cs
@@ -39,10 +39,13 @@ namespace DesktopContactsApp
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-            connection.Insert(contact);
-            connection.Close();
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+                
+            
             Close();
         }
     }

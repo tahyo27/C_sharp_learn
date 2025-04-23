d68229a0caf76cef8d21b704227e222879a1ddf8
tahyo27 <tahyo27@gmail.com>
2025-04-23 20:29:09 +0900

    C# WPF binding custom control

--- DesktopContactsApp/DesktopContactsApp/Controls/ContactControl.xaml.cs
+++ DesktopContactsApp/DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -21,19 +21,31 @@ namespace DesktopContactsApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
 
-        public Contact Contact 
+
+        public Contact Contact
         {
-            get { return contact; }
-            set 
-            { 
-                contact = value;
-                nameTextBlock.Text = contact.Name;
-                emailTextBlock.Text = contact.Email;
-                phoneTextBlock.Text = contact.Phone;
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact() {Name = "Name Lastname",
+                Phone = "(123) 456 789", Email = "example@gmail.com"}, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+
+            if(control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
             }
         }
+
         public ContactControl()
         {
             InitializeComponent();

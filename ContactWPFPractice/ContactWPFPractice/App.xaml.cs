26f9efcf3a62a1da53049d9bd63cdb987c59b641
tahyo27 <tahyo27@gmail.com>
2025-04-28 20:39:17 +0900

    C# WPF Contact practice connect DB and ListView

--- ContactWPFPractice/ContactWPFPractice/App.xaml.cs
+++ ContactWPFPractice/ContactWPFPractice/App.xaml.cs
@@ -9,5 +9,8 @@ namespace ContactWPFPractice;
 /// </summary>
 public partial class App : Application
 {
+    static string databaseName = "ContactTest.db";
+    static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 }
 
--- ContactWPFPractice/ContactWPFPractice/MainWindow.xaml.cs
+++ ContactWPFPractice/ContactWPFPractice/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ContactWPFPractice.Model;
 
 namespace ContactWPFPractice;
 
@@ -16,9 +18,25 @@ namespace ContactWPFPractice;
 /// </summary>
 public partial class MainWindow : Window
 {
+    List<Contact> contacts;
     public MainWindow()
     {
         InitializeComponent();
-        
+        contacts = new List<Contact>();
+        ReadDataBase();
+    }
+
+    private void ReadDataBase()
+    {
+        List<Contact> contacts;
+        using(SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+        {
+            conn.CreateTable<Contact>();
+            contacts = [.. (conn.Table<Contact>().ToList()).OrderBy(c => c.Name)];
+        }
+        if(contacts is not null)
+        {
+            contactList.ItemsSource = contacts;
+        }
     }
 }

4100b08f5ec66de8bfa84340199b68621fbd0581
tahyo27 <tahyo27@gmail.com>
2025-04-29 21:03:52 +0900

    C# WPF Contact practice create new window

---
+++ ContactWPFPractice/ContactWPFPractice/NewContactWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using ContactWPFPractice.Model;
+using SQLite;
+
+namespace ContactWPFPractice
+{
+    /// <summary>
+    /// NewContactWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = NameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextBox.Text,
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+
+    }
+}

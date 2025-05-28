d658ffe544ce89615fe8900f003dd13daef73bcf
tahyo27 <tahyo27@gmail.com>
2025-05-28 22:02:40 +0900

    C# WPF Weather MVVM add accuweather class

---
+++ WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "";
+        public const string AUTOCOMPLETE_ENDPOINT = "";
+    }
+}

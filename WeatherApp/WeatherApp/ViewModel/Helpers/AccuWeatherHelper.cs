9b5dad0e574d68bd72b3b749a75a3de316f1f393
tahyo27 <tahyo27@gmail.com>
2025-05-30 23:40:22 +0900

    C# WPF weatherapp add current conditions endpoint

--- WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -10,6 +10,7 @@ namespace WeatherApp.ViewModel.Helpers
     {
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey{0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
         public const string API_KEY = "t9VWTvQ2YBP1QwHAeyaErCCZoPCoBcSo";
     }
 }

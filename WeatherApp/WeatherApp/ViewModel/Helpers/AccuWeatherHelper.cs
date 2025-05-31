c52347af41272ff5e9b00a97a62732426289d291
tahyo27 <tahyo27@gmail.com>
2025-05-31 21:41:27 +0900

    C# WPF weatherApp add get cities http

--- WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Model;
 
 namespace WeatherApp.ViewModel.Helpers
 {
     public class AccuWeatherHelper
     {
         public const string BASE_URL = "http://dataservice.accuweather.com/";
-        public const string AUTOCOMPLETE_ENDPOINT = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey{0}&q={1}";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey{0}&q={1}";
         public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
         public const string API_KEY = "t9VWTvQ2YBP1QwHAeyaErCCZoPCoBcSo";
+
+        public static async List<City> Getcities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<>(json);
+            }
+
+            return cities;
+        }
     }
 }

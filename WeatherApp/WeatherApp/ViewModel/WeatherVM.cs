866e14948ab5276a97386ddcb2bb05abf04ae5c0
tahyo27 <tahyo27@gmail.com>
2025-06-11 21:15:42 +0900

    C# WPF WeatherApp binding context1

--- WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -5,10 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeatherApp.View
+namespace WeatherApp.ViewModel
 {
     public class WeatherVM : INotifyPropertyChanged
     {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged(string propertyName)

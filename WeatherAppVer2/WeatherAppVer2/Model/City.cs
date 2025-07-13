c463ff3e6b1751c5235191ae839e5ccdd478f317
tahyo27 <tahyo27@gmail.com>
2025-07-13 17:46:02 +0900

    C# WPF weather app ver2 add city class

---
+++ WeatherAppVer2/WeatherAppVer2/Model/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppVer2.Model
+{
+    public class City
+    {
+    }
+}

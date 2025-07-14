b8007c5011e4fbf4fa0e50fe11dc4b2b98d1ceab
tahyo27 <tahyo27@gmail.com>
2025-07-14 22:43:47 +0900

    C# add weather props

--- WeatherAppVer2/WeatherAppVer2/Model/City.cs
+++ WeatherAppVer2/WeatherAppVer2/Model/City.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace WeatherAppVer2.Model
 {
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+    
     public class City
     {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName {  get; set; }
+
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
     }
 }

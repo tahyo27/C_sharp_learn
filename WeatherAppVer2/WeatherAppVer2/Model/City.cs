e84b604dd690acc3df1f690e4aec595cd12a961d
tahyo27 <tahyo27@gmail.com>
2025-07-15 22:55:43 +0900

    Add Current Condition weatherApp

--- WeatherAppVer2/WeatherAppVer2/Model/City.cs
+++ WeatherAppVer2/WeatherAppVer2/Model/City.cs
@@ -11,15 +11,14 @@ namespace WeatherAppVer2.Model
         public string ID { get; set; }
         public string LocalizedName { get; set; }
     }
-    
+
     public class City
     {
         public int Version { get; set; }
         public string Key { get; set; }
         public string Type { get; set; }
         public int Rank { get; set; }
-        public string LocalizedName {  get; set; }
-
+        public string LocalizedName { get; set; }
         public Area Country { get; set; }
         public Area AdministrativeArea { get; set; }
     }

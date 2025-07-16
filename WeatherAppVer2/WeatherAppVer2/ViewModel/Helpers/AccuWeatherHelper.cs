01ab15e7af461ff13a7ac65b05a5a1fa14fe92c5
tahyo27 <tahyo27@gmail.com>
2025-07-16 18:41:53 +0900

    C# WPF add WeatherVer2 add helpers

---
+++ WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppVer2.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+    }
+}

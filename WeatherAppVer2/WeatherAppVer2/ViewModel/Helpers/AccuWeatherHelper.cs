ce39f95c5275cca34d41cacf8303a139c0e59e9c
tahyo27 <tahyo27@gmail.com>
2025-07-17 21:14:06 +0900

    C# WPF add get cities

--- WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherAppVer2.Model;
 
 namespace WeatherAppVer2.ViewModel.Helpers
 {
     public class AccuWeatherHelper
     {
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
     }
 }

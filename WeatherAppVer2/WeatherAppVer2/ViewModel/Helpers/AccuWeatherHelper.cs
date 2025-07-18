5385f53cb37a60062a2ff2e9f33085e73c18e187
tahyo27 <tahyo27@gmail.com>
2025-07-18 20:30:14 +0900

    C# Weatherver2 add const string

--- WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -11,8 +11,15 @@ namespace WeatherAppVer2.ViewModel.Helpers
 {
     public class AccuWeatherHelper
     {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "Sx1OF3piASbTJyfXPVv0HJm65AWLnTV6";
+        //TODO : APIKEY 변경
         public static async Task<List<City>> GetCities(string query)
         {
+            
+
             List<City> cities = new List<City>();
 
             string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);

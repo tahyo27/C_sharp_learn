a7cc114bb68d7b5eca6709a256cc7ed86a4f807e
tahyo27 <tahyo27@gmail.com>
2025-07-20 21:13:57 +0900

    C# WeatherAppver2 add apikey and newtonjson

--- WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherAppVer2/WeatherAppVer2/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using WeatherApp.Model;
 using WeatherAppVer2.Model;
 
@@ -15,7 +16,7 @@ namespace WeatherAppVer2.ViewModel.Helpers
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
         public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
-        public const string API_KEY = "Sx1OF3piASbTJyfXPVv0HJm65AWLnTV6";
+        public const string API_KEY = "t9VWTvQ2YBP1QwHAeyaErCCZoPCoBcSo";
         //TODO : APIKEY 변경
         public static async Task<List<City>> GetCities(string query)
         {

949ec9c06db06f368a887d4dad89d041d9270e07
tahyo27 <tahyo27@gmail.com>
2026-01-12 21:29:52 +0900

    add calprac method

--- CalPractice/CalPractice/MainWindow.xaml.cs
+++ CalPractice/CalPractice/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace CalPractice
     /// </summary>
     public partial class MainWindow : Window
     {
-        double lastNumber;
+        double lastNumber, result;
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,12 +33,71 @@ namespace CalPractice
             resultLabel.Content = "0";
             acButton.Click += AcButton_Click;
             nagativeButton.Click += NagativeButton_Click;
+            percentButton.Click += PercentButton_Click;
+            equalButton.Click += EqualButton_Click;
+
+            zeroButton.Click += NumberButton_Click;
+            oneButton.Click += NumberButton_Click;
+            twoButton.Click += NumberButton_Click;
+            threeButton.Click += NumberButton_Click;
+            fourButton.Click += NumberButton_Click;
+            fiveButton.Click += NumberButton_Click;
+            sixButton.Click += NumberButton_Click;
+            sevenButton.Click += NumberButton_Click;
+            eightButton.Click += NumberButton_Click;
+            nineButton.Click += NumberButton_Click;
+
+            pointButton.Click += PointButton_Click;
         }
 
+        private void PointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!resultLabel.Content.ToString().Contains("."))
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Sustraction:
+                        result = SimpleMath.Sustraction(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
 
         private void NagativeButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
         }
 
         private void AcButton_Click(object sender, RoutedEventArgs e)
@@ -49,19 +109,23 @@ namespace CalPractice
         {
             if(sender is Button btn)
             {
-                int.TryParse(resultLabel.Content.ToString(), out int result);
-
-                switch(btn.Name)
+                if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
                 {
-                    case "multiplyButton":
-                        break;
+                    resultLabel.Content = "0";
                 }
+                if(sender == multiplyButton)
+                    selectedOperator = SelectedOperator.Multiplication;
+                if (sender == divideButton)
+                    selectedOperator = SelectedOperator.Division;
+                if (sender == plusButton)
+                    selectedOperator = SelectedOperator.Addition;
+                if (sender == minusButton)
+                    selectedOperator = SelectedOperator.Sustraction;
             }
 
 
         }
-
-        private void NumberButtonClick(object sender, RoutedEventArgs e)
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
             if(sender is Button btn)
             {
@@ -77,4 +141,32 @@ namespace CalPractice
             }
         }
     }
+    public enum SelectedOperator
+    {
+        Addition,
+        Sustraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Sustraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+
 }

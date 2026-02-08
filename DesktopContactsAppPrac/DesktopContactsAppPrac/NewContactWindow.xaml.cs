582a5fbc1a01455bb10247536cc357298f7a407b
tahyo27 <tahyo27@gmail.com>
2026-02-08 15:08:13 +0900

    add contact using

--- DesktopContactsAppPrac/DesktopContactsAppPrac/NewContactWindow.xaml.cs
+++ DesktopContactsAppPrac/DesktopContactsAppPrac/NewContactWindow.xaml.cs
@@ -39,12 +39,11 @@ namespace DesktopContactsAppPrac
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-            connection.Insert(contact);
-            connection.Close();
-
-
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
 
             Close();
         }
